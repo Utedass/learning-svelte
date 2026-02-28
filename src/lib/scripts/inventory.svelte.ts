5d0f10923aa6e912285338525883e0b064749c10
Jonatan Gezelius <boolean.adapter@gmail.com>
2026-03-01 00:01:22 +0100

    Add some basic start for the inventory fetching.

---
+++ src/lib/scripts/inventory.svelte.ts
@@ -0,0 +1,101 @@
+import { globalSettings } from '$scripts/types/settings.svelte';
+
+export const reply = $state({ content: 'empty' });
+
+export const tass = $state([{ tass: 2 }, { tass: 4 }]);
+
+export async function getSomething() {
+	console.log('Whhaaaaa');
+	const myHeaders = new Headers();
+
+	myHeaders.append('GROCY-API-KEY', globalSettings.APIkey);
+
+	const requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow' as RequestRedirect
+	};
+
+	let response: Response;
+	try {
+		response = await fetch(globalSettings.BaseUrl + '/objects/products', requestOptions);
+		reply.content = JSON.stringify(await response.json(), null, 2);
+	} catch (err) {
+		console.log('Something sucked.');
+	}
+}
+
+export const products = $state<Record<number, Product>>({});
+
+export type ProductsResponse = Product[];
+
+export type Product = {
+	id: number;
+	name: string;
+	description: string | null;
+	product_group_id: number;
+	active: number;
+	location_id: number;
+	shopping_location_id: number | null;
+	qu_id_purchase: number;
+	qu_id_stock: number;
+	min_stock_amount: number;
+	default_best_before_days: number;
+	default_best_before_days_after_open: number;
+	default_best_before_days_after_freezing: number;
+	default_best_before_days_after_thawing: number;
+	picture_file_name: string;
+	enable_tare_weight_handling: number;
+	tare_weight: number;
+	not_check_stock_fulfillment_for_recipes: number;
+	parent_product_id: number | null;
+	calories: number;
+	cumulate_min_stock_amount_of_sub_products: number;
+	due_type: number;
+	quick_consume_amount: number;
+	hide_on_stock_overview: number;
+	default_stock_label_type: number;
+	should_not_be_frozen: number;
+	treat_opened_as_out_of_stock: number;
+	no_own_stock: number;
+	default_consume_location_id: number;
+	move_on_open: number;
+	row_created_timestamp: string;
+	qu_id_consume: number;
+	auto_reprint_stock_label: number;
+	quick_open_amount: number;
+	qu_id_price: number;
+	disable_open: number;
+	default_purchase_price_type: number;
+};
+
+export async function fetchProducts() {
+	console.log('Trying to populate some products');
+
+	const myHeaders = new Headers();
+	myHeaders.append('GROCY-API-KEY', globalSettings.APIkey);
+
+	const params = new URLSearchParams();
+	params.append('limit', '3');
+
+	const requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow' as RequestRedirect
+	};
+
+	try {
+		const res = await fetch(globalSettings.BaseUrl + `/objects/products?${params}`, requestOptions);
+		const list: Product[] = await res.json();
+
+		const productRecord: Record<number, Product> = {};
+
+		for (const p of list) {
+			productRecord[p.id] = p;
+		}
+		Object.assign(products, productRecord);
+		console.log(`Retrieved ${Object.keys(productRecord).length} products`);
+	} catch (err) {
+		console.log('Something sucked.');
+	}
+}
--- src/routes/getstuff/+page.svelte
+++ src/routes/getstuff/+page.svelte
@@ -3,6 +3,8 @@
 	import { Settings as SettingsIcon } from '@lucide/svelte';
 	import type { Settings } from '$scripts/types/settings.svelte.ts';
 	import { globalSettings } from '$scripts/types/settings.svelte.ts';
+	import type { Product } from '$scripts/inventory.svelte.ts';
+	import { reply, getSomething, tass, fetchProducts, products } from '$scripts/inventory.svelte.ts';
 
 	function onOpenChange() {
 		console.log('State changed:', visible);
@@ -10,6 +12,18 @@
 </script>
 
 <h1 class="h1">Lets get some stuff.</h1>
+
+<p>{products.length}</p>
+<button class="btn preset-filled" onclick={fetchProducts}>Get some products!!</button>
+
+{#each Object.values(products) as p}
+	<div class="card">
+		<div>{p.id}</div>
+		<div>{p.name}</div>
+	</div>
+	<hr />
+{/each}
+<button class="btn preset-filled" onclick={() => (products[4].name += ' HA')}>Ändra nåt!</button>
 
 <style>
 	h1 {
