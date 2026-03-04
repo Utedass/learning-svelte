3f01e9d4e9adb85556442594d13cc38fa82461f2
Jonatan Gezelius <boolean.adapter@gmail.com>
2026-03-04 23:36:38 +0100

    Move all routes down one folder to avoid global +layout

--- src/lib/components/nav-bar.svelte
+++ src/lib/components/nav-bar.svelte
@@ -5,16 +5,16 @@
 
 <div class="flex preset-filled-secondary-100-900 p-4">
 	<div class="flex flex-wrap items-center gap-4">
-		<a href="/">home</a>
-		<a href="/about">about</a>
-		<a href="/trystuff">trystuff</a>
-		<a href="/slugstuff">slugstuff</a>
-		<a href="/layouts1">layouts1</a>
-		<a href="/layouts2">layouts2</a>
-		<a href="/getstuff">getstuff</a>
-		<a href="/hierarchy">Grocy parents</a>
-		<a href="/zebra">Zebra</a>
-		<a href="/404">404</a>
+		<a href="/pages/./">home</a>
+		<a href="/pages/about">about</a>
+		<a href="/pages/trystuff">trystuff</a>
+		<a href="/pages/slugstuff">slugstuff</a>
+		<a href="/pages/layouts1">layouts1</a>
+		<a href="/pages/layouts2">layouts2</a>
+		<a href="/pages/getstuff">getstuff</a>
+		<a href="/pages/hierarchy">Grocy parents</a>
+		<a href="/pages/zebra">Zebra</a>
+		<a href="/pages/404">404</a>
 	</div>
 	<div class="ml-auto flex flex-wrap-reverse gap-4 items-center">
 		<LightDarkToggle />
