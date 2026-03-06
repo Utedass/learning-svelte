8d8962f769d7d6f22a1705b555f89442f093b8dd
Jonatan Gezelius <jonatan.gezelius@etteplan.com>
2026-03-06 07:56:04 +0100

    Add blank experiments folder

---
+++ src/routes/blank/1/+page.svelte
@@ -0,0 +1,18 @@
+<div id="content">
+content
+</div>
+
+<style>
+	* {
+		border: 0.7px solid black;
+	}
+
+	#content {
+		max-width: 1280px;
+		margin: 0px auto;
+
+		display: flex;
+		flex-direction: column;
+		gap: 1ex;
+	}
+</style>
