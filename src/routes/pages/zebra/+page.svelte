755cdd37dfdb0a60508bd9bfb23e87d242266f15
Jonatan Gezelius <boolean.adapter@gmail.com>
2026-03-08 21:56:22 +0100

    Add backend call to print from zebra printer.

--- src/routes/pages/zebra/+page.svelte
+++ src/routes/pages/zebra/+page.svelte
@@ -1,45 +1,15 @@
 <script lang="ts">
-	import net from 'net';
+	import { enhance } from '$app/forms';
+	import type { PageProps } from './$types';
+	let { form }: PageProps = $props();
 
-	function printLabel() {
-		const printerHost = 'd6j235201761.home.arpa';
-		const printerPort = 9100;
-
-		const now = new Date();
-		const timestring = now.toISOString().slice(0, 10); // "YYYY-MM-DD"
-
-		const zplCommand = `
-^XA
-^LH16,8
-^FO100,15
-^AB,20
-^FDFrysdag^FS
-^FO50,50
-^A0,40
-^FD${timestring}^FS
-^LH0,0
-^XZ
-`;
-
-		const client = new net.Socket();
-
-		client.connect(printerPort, printerHost, () => {
-			console.log('Connected to printer!');
-			client.write(zplCommand, 'utf8', () => {
-				console.log('Sent ZPL to printer!');
-				client.end();
-			});
-		});
-
-		client.on('error', (err) => {
-			console.error('Connection failed:', err);
-		});
-
-		client.on('close', () => {
-			console.log('Connection closed');
-		});
-	}
 </script>
 
 Nope, this will not work. Have to make a proxy..
-<button class="btn preset-filled" onclick={printLabel}>Skriv ut!</button>
+
+
+<hr />
+<form method="POST" use:enhance>
+	<input type="string" class="input" name="stringJohan" />
+	<button type="submit" class="btn preset-filled">Tjoho!</button>
+</form>
